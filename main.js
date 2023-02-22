80c5098f85b4ce8df0d3605db08c7f66747b780f
m3goody <116851784+m3goody@users.noreply.github.com>
2023-02-21 19:55:32 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("My Console Project");
+
+var first="Timothy";
+var last="Jung";
+var fullname=first.concat(last);
+console.log(fullname);
+
+var a=10;
+var b=5;
+var add=a+b;
+console.log(add);
+
+var sub=a-b;
+console.log(sub);
+
+var mul=a*b;
+console.log(mul);
+
+var div=a/b;
+console.log(div);
